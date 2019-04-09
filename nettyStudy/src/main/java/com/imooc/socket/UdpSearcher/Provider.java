16564438b42cff7d89be74b31c09d7f6766141e3
wangjingbiao <577322753@qq.com>
2019-04-09 14:57:41 +0800

    使用udp实现一个简单的广播搜索设备

--- nettyStudy/src/main/java/com/imooc/socket/UdpSearcher/Provider.java
+++ nettyStudy/src/main/java/com/imooc/socket/UdpSearcher/Provider.java
@@ -3,6 +3,7 @@ package com.imooc.socket.UdpSearcher;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.UUID;
 
 /**
  * @PackageName com.imooc.socket.baseUdpDemo
@@ -13,15 +14,21 @@ import java.net.DatagramSocket;
  */
 public class Provider {
     public static void main(String[] args) throws IOException {
+        String sn = UUID.randomUUID().toString();
+        ReceiveRunner runner = new ReceiveRunner(sn);
+        new Thread(runner).start();
 
+        // 读取任意字符退出程序
+        System.in.read();
+        runner.exit();
     }
 
-    private class receiveRunner implements Runnable {
+    private static class ReceiveRunner implements Runnable {
         private final String sn;
         private boolean done = false;
         private DatagramSocket ds = null;
 
-        public receiveRunner(String sn) {
+        public ReceiveRunner(String sn) {
             this.sn = sn;
         }
 
@@ -30,9 +37,10 @@ public class Provider {
             try {
                 // 构建监听
                 ds = new DatagramSocket(20000);
-                while (!done) {
-                    System.out.println("udp服务方启动");
+                System.out.println("服务方监听启动");
 
+                // 持续接收数据
+                while (!done) {
                     // 接收数据
                     final byte[] buf = new byte[512];
                     DatagramPacket receivePack = new DatagramPacket(buf, buf.length);
@@ -45,9 +53,14 @@ public class Provider {
                     String data = new String(receivePack.getData(), 0, dataLen);
                     System.out.printf("UDPProvider receive from ip: %s ,port: %s ,data: %s\n", ip, port, data);
 
+                    // 从消息包里，解析出指定的回传端口
                     int responsePort = MessageCreator.parsePort(data);
-                    if (responsePort != -1){
-
+                    if (responsePort != -1) {
+                        // 证明给出的端口是有效的，回送数据
+                        String responseData = MessageCreator.buildWithSn(sn);
+                        DatagramPacket responsePacket = new DatagramPacket(responseData.getBytes(),
+                                responseData.getBytes().length,receivePack.getAddress(),responsePort);
+                        ds.send(responsePacket);
                     }
                 }
             } catch (Exception e) {
@@ -55,6 +68,7 @@ public class Provider {
             } finally {
                 close();
             }
+            System.out.println("服务方监听结束");
 
         }
 
@@ -67,6 +81,7 @@ public class Provider {
 
         void exit() {
             done = true;
+            close();
         }
     }
 }
--- nettyStudy/src/main/java/com/imooc/socket/UdpSearcher/Searcher.java
+++ nettyStudy/src/main/java/com/imooc/socket/UdpSearcher/Searcher.java
@@ -1,8 +1,10 @@
 package com.imooc.socket.UdpSearcher;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @PackageName com.imooc.socket.baseUdpDemo
@@ -12,34 +14,156 @@ import java.net.InetAddress;
  * @Description udp搜索者，用于搜索服务提供方
  */
 public class Searcher {
+    private static final int LISTEN_PORT = 30000;
+
     public static void main(String[] args) throws Exception {
+        System.out.println("搜索者开始");
         // 服务者的ip
-        int searchPort = 20000;
-        System.out.println("udp搜索方启动");
+        //开启监听
+        Listener listen = listen();
+        // 发送广播，等待回复
+        sendBroadcast();
 
-        // 作为搜索方，让系统自动分配
+        System.in.read();
+        List<Device> devices = listen.getDevicesAndClose();
+        for (Device device : devices) {
+            System.out.println(device.toString());
+        }
+        System.out.println("搜索者退出");
+    }
+
+    /**
+     * 监听
+     */
+    public static Listener listen() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        Listener listener = new Listener(LISTEN_PORT, countDownLatch);
+        new Thread(listener).start();
+        // 必须等待开始监听后，再发送广播消息
+        countDownLatch.await();
+        return listener;
+
+    }
+
+    private static class Device {
+        private int port;
+        private String ip;
+        private String sn;
+
+        public Device(int port, String ip, String sn) {
+            this.port = port;
+            this.ip = ip;
+            this.sn = sn;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getSn() {
+            return sn;
+        }
+
+        public void setSn(String sn) {
+            this.sn = sn;
+        }
+
+        @Override
+        public String toString() {
+            return "Device{" +
+                    "port=" + port +
+                    ", ip='" + ip + '\'' +
+                    ", sn='" + sn + '\'' +
+                    '}';
+        }
+    }
+
+    /**
+     * 发送广播
+     */
+    public static void sendBroadcast() throws IOException {
+        System.out.println("搜索方开始发送广播");
+        // 作为搜索方，让系统自动分配端口
         DatagramSocket ds = new DatagramSocket();
+        // 广播请求报文
+        String requestData = MessageCreator.buildWithPort(LISTEN_PORT);
+        DatagramPacket requestPacket = new DatagramPacket(requestData.getBytes(), requestData.getBytes().length);
+        requestPacket.setAddress(InetAddress.getByName("255.255.255.255"));
+        requestPacket.setPort(20000);
+        //发送搜索报文
+        ds.send(requestPacket);
+        System.out.println("搜索方发送广播结束");
+    }
 
-        // 构建报文，并发送消息
-        String data = "hello,world! i am UDP";
-        DatagramPacket requestPack = new DatagramPacket(data.getBytes(), data.getBytes().length);
-        requestPack.setAddress(InetAddress.getLocalHost());
-        requestPack.setPort(searchPort);
-        ds.send(requestPack);
-        System.out.println("发送消息成功");
+    private static class Listener implements Runnable {
+        public final int listenPort;
+        private final CountDownLatch countDownLatch;
+        private final List<Device> devices = new ArrayList<>();
+        private boolean done = false;
+        private DatagramSocket ds = null;
 
+        public Listener(int listenPort, CountDownLatch countDownLatch) {
+            this.listenPort = listenPort;
+            this.countDownLatch = countDownLatch;
+        }
 
-        // 接收数据
-        final byte[] buf = new byte[512];
-        DatagramPacket receiveResponsePack = new DatagramPacket(buf, buf.length);
-        ds.receive(receiveResponsePack);
+        @Override
+        public void run() {
+            System.out.println("搜索方开始监听");
+            //通知已经启动
+            countDownLatch.countDown();
+            try {
+                ds = new DatagramSocket(listenPort);
+                while (!done) {
+                    // 接收数据
+                    final byte[] buf = new byte[512];
+                    DatagramPacket receivePack = new DatagramPacket(buf, buf.length);
+                    ds.receive(receivePack);
 
-        // 接收到消息后取出所有信息
-        String ip = receiveResponsePack.getAddress().getHostAddress();
-        int port = receiveResponsePack.getPort();
-        int dataLen = receiveResponsePack.getLength();
-        String receiveResponseData = new String(receiveResponsePack.getData(), 0, dataLen);
-        System.out.printf("UDPProvider receive from ip: %s ,port: %s ,data: %s\n",ip,port,receiveResponseData);
-        ds.close();
+                    // 接收到消息后取出所有信息
+                    String ip = receivePack.getAddress().getHostAddress();
+                    int port = receivePack.getPort();
+                    int dataLen = receivePack.getLength();
+                    String data = new String(receivePack.getData(), 0, dataLen);
+                    System.out.printf("Searcher receive from ip: %s ,port: %s ,data: %s\n", ip, port, data);
+
+                    String sn = MessageCreator.parseSn(data);
+                    if (sn != null) {
+                        Device device = new Device(port, ip, sn);
+                        devices.add(device);
+                    }
+                }
+            } catch (Exception e) {
+
+            } finally {
+
+            }
+            System.out.println("搜索方监听结束");
+        }
+
+        public void close() {
+            if (ds != null) {
+                ds.close();
+                ds = null;
+            }
+        }
+
+        List<Device> getDevicesAndClose() {
+            done = true;
+            close();
+            return devices;
+        }
     }
 }
